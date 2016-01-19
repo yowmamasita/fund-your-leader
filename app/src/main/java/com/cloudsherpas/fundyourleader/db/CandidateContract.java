dbaffa26d916d553d41018ace430f593d0d6b852
Ben Sarmiento <me@bensarmiento.com>
2016-01-19 17:05:02 +0800

    Models

---
+++ app/src/main/java/com/cloudsherpas/fundyourleader/db/CandidateContract.java
@@ -0,0 +1,16 @@
+package com.cloudsherpas.fundyourleader.db;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by ben on 1/19/16.
+ */
+public class CandidateContract {
+    public CandidateContract() {}
+
+    public static abstract class CandidateEntry implements BaseColumns {
+        public static final String TABLE_NAME = "candidate";
+        public static final String _ID = "id";
+        public static final String COLUMN_NAME_NAME = "name";
+    }
+}
---
+++ app/src/main/java/com/cloudsherpas/fundyourleader/db/CandidateDbHelper.java
@@ -0,0 +1,39 @@
+package com.cloudsherpas.fundyourleader.db;
+
+/**
+ * Created by ben on 1/19/16.
+ */
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.content.Context;
+
+
+public class CandidateDbHelper extends SQLiteOpenHelper {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "FYL.db";
+
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + CandidateContract.CandidateEntry.TABLE_NAME + " (" +
+                    CandidateContract.CandidateEntry._ID + " INTEGER PRIMARY KEY," +
+                    CandidateContract.CandidateEntry.COLUMN_NAME_NAME + " TEXT" +
+            " )";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + CandidateContract.CandidateEntry.TABLE_NAME;
+
+    public CandidateDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+}
