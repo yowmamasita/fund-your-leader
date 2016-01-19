97485b3df3486f2adb6f657aa7153fa801d0a4e1
Abraham Nazer Padilla <abraham.padilla@cloudsherpas.com>
2016-01-19 16:52:31 +0800

    Layouts for Tab View and Pledge Amounts List

---
+++ app/src/main/java/com/cloudsherpas/fundyourleader/PledgeAmountListActivity.java
@@ -0,0 +1,13 @@
+package com.cloudsherpas.fundyourleader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PledgeAmountListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pledge_amount_list);
+    }
+}
