4d3e1c23652bc2532c64c4aa462f6a790d50453e
Ben Sarmiento <me@bensarmiento.com>
2016-01-19 17:04:35 +0800

    DB helpers and model

--- app/src/main/java/com/cloudsherpas/fundyourleader/FullscreenActivity.java
+++ app/src/main/java/com/cloudsherpas/fundyourleader/FullscreenActivity.java
@@ -1,9 +1,13 @@
 package com.cloudsherpas.fundyourleader;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 
+import com.cloudsherpas.fundyourleader.db.CandidateContract;
+import com.cloudsherpas.fundyourleader.db.CandidateDbHelper;
 import com.firebase.client.Firebase;
 
 /**
@@ -17,6 +21,15 @@ public class FullscreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fullscreen);
         Firebase.setAndroidContext(this);
+        CandidateDbHelper mDbHelper = new CandidateDbHelper(getApplicationContext());
+
+        // Gets the data repository in write mode
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        // Create a new map of values, where column names are the keys
+        ContentValues values = new ContentValues();
+        values.put(CandidateContract.CandidateEntry.COLUMN_NAME_NAME, "Binay");
+        db.insert(CandidateContract.CandidateEntry.TABLE_NAME, "", values);
     }
 
     @Override
