ad1e9a42eeff02fbf63b0621670fd7b3378e06e4
Abraham Nazer Padilla <abraham.padilla@cloudsherpas.com>
2016-01-20 19:31:20 +0800

    List view for candidates' campaign expenses

--- app/src/main/java/com/cloudsherpas/fundyourleader/CandidateActivity.java
+++ app/src/main/java/com/cloudsherpas/fundyourleader/CandidateActivity.java
@@ -3,27 +3,22 @@ package com.cloudsherpas.fundyourleader;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -283,9 +278,24 @@ public class CandidateActivity extends AppCompatActivity {
         @Nullable
         @Override
         public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-//            View view = inflater.inflate(R.layout.page1, null);
-//            return view;
-            return super.onCreateView(inflater, container, savedInstanceState);
+
+            View view = inflater.inflate(R.layout.campaign_expense_list, null);
+            ListView campaignExpenseListView = (ListView) view.findViewById(R.id.listView3);
+
+            String campaignExpense1 = "Spent P250000 on Media advertisements.";
+            String campaignExpense2 = "Spent P100000 on Rental of Campaign Venues";
+            String campaignExpense3 = "Spent P138000 on Transportation";
+            String campaignExpense4 = "Spent P206900 on Security Purposes";
+
+            String[] pledgeAmounts = new String[]{ campaignExpense1, campaignExpense2, campaignExpense3, campaignExpense4 };
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                    getContext(), android.R.layout.simple_list_item_activated_1, pledgeAmounts
+            );
+
+            campaignExpenseListView.setAdapter(adapter);
+            return view;
+
+            //return super.onCreateView(inflater, container, savedInstanceState);
         }
     }
 
